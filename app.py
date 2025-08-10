aedf0350241b2d652675ebcbabac129e92189cdf
Shravani kodag <139679583+shravanikodag@users.noreply.github.com>
2025-08-10 20:51:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# About page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Contact page
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
